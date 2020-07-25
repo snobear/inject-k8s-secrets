0cb448b95da929096020ea866264b4905ee2e881
Jason Ashby <jashby2@gmail.com>
2020-07-25 09:33:53 -0400

    add bash script

---
+++ inject.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# Iterate through list of all secrets in AWS Secrets Manager and inject into cluster as k8s secrets
+
+# CHANGE ME to the namespace your secrets live under in Secrets Manager
+secret_prefix="myapp/dev"
+
+if [[ $# -ne 4 ]] ; then
+    echo "usage: $0 cluster namespace region profile" >&2
+    exit 2
+fi
+
+cluster=$1
+namespace=$2
+AWS_REGION=$3
+AWS_PROFILE=$4
+
+# kubectl client 1.18 introduced some breaking changes with --dry-run
+kubectl_ver=$(kubectl version --client=true -o json | jq -rj '.clientVersion | .major, ".", .minor')
+dry_run_flag="--dry-run"
+if [[ "$ver_major" -gt "1" ]] || [[ "$ver_minor" -gt "17" ]]; then
+    dry_run_flag="--dry-run=client"
+fi
+
+echo "Injecting all secrets under ${secret_prefix} from AWS Secrets Manager into cluster ${cluster}, namespace ${namespace}"
+
+secret_count=0
+
+# iterate through list of all secrets in AWS Secrets Manager for this stack
+for secret_name in $(aws secretsmanager list-secrets --profile ${AWS_PROFILE} --region ${AWS_REGION} --query 'SecretList[?Name!=`null`]|[?starts_with(Name, `'${secret_prefix}'`) == `true`].Name' --output text); do
+    secret_count=$((secret_count+1))
+
+    if [[ $secret_name == "None" ]]; then
+        echo "error: aws secrets manager list-secrets returned None."
+        exit 1
+    fi
+
+    unset k8s_secret_name value
+
+    # make a k8s secrets-friendly name ($service-$secretname)
+    echo "secret name: $secret_name"
+    k8s_secret_name=$(echo ${secret_name#"$secret_prefix"/} | tr "/_" "-")
+    if [[ -z $k8s_secret_name ]]; then
+        echo "warning: k8s_secret_name empty for secret_name=$secret_name"
+    fi
+
+    value=$(aws secretsmanager get-secret-value --secret-id ${secret_name} --query 'SecretString' --output text --region ${AWS_REGION})
+ 
+    if [[ -z $value ]]; then
+        echo "warning: secret value is empty for secret_name=${secret_name}. skipping k8s secret injection."
+    else
+        if [[ ${secret_count} -eq 1 ]]; then
+            # table header
+            echo
+            line=$(printf -- '=%.0s' {1..20}; echo "")
+            printf "%-65s----> %s\n" "AWS Secret name" "k8s Secret Name"
+            printf "%-70s %s\n" ${line} ${line}
+        fi
+        printf "%-70s %s\n" ${secret_name} ${k8s_secret_name}
+
+        # this is currently the best method to "upsert" a secret, other than deleting and recreating it.
+        #kubectl create secret generic ${k8s_secret_name} --from-literal=password=${value} -n ${stack} ${dry_run_flag} -o yaml | kubectl apply -f - > /dev/null
+    fi
+done
+
+unset value
+
+if [[ $secret_count -eq 0 ]]; then
+    echo "No secrets found in AWS Secrets Manager for ${stack}."
+fi
